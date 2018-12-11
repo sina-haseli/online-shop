cb9ebd0dce187e2b72640a467f97f9e14398924e
sina-haseli <40861222+sina-haseli@users.noreply.github.com>
2018-12-11 21:49:07 +0330

    COMMIT MESSAGE

---
+++ src/setAuthToken.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const setAuthToken = token => {
+    if(token) {
+        axios.defaults.headers.common['x-access-token'] = "Bearer " + token;
+    }
+    else {
+        delete axios.defaults.headers.common['x-access-token'];
+    }
+};
+
+export default setAuthToken;
